6adea43a8083d46b845251497d683c0abec3496f
dcoleman44 <dcoleman44@outlook.com>
2018-09-16 21:37:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+fizz_3 = fizzbuzz(3)
+fizz_5 = fizzbuzz(5)
+fizz_15 = fizzbuzz(15)
+
+def fizzbuzz()
+  if 
+    return "Fizz"
+  elsif fizz_5
+    return "Buzz"
+  elsif fizz_15
+    return "FizzBuzz"
+  else
+    return nil
+  end
+end
