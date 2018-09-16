2f7a96d925e8632e5093c6bb86400caf0486eaa7
dcoleman44 <dcoleman44@outlook.com>
2018-09-16 21:49:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-fizz_3 = fizzbuzz(3)
-fizz_5 = fizzbuzz(5)
-fizz_15 = fizzbuzz(15)
-fizz_4 = fizzbuzz(4)
-def fizzbuzz()
+
+def fizzbuzz(fizz_3, fizz_5, fizz_15, fizz_4)
   if int % 3 == 0 and int % 5 == 0
     return "FizzBuzz"
   elsif int % 3 == 0
@@ -15,3 +12,6 @@ def fizzbuzz()
     return nil
   end
 end
+
+fizzbuzz(3, 5, 15, 4)
+
